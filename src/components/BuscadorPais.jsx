a313b7ef7d4d4f15798eb67cb108cec8c74348fc
Luciano Angeleri <lcnang@gmail.com>
2023-06-09 03:17:36 -0300

    Se añadió toda la lógica para obtener los datos de la API

--- src/components/BuscadorPais.jsx
+++ src/components/BuscadorPais.jsx
@@ -3,50 +3,69 @@ import DatosClima from './DatosClima';
 import { useState, useEffect } from "react";
 
 const BuscadorPais = () => {
-    const [ubicacion, setUbicacion] = useState("");
-      const [climas, setClimas] = useState([]);
+    const [ciudad, setCiudad] = useState("");
+    const [ubicacion, setUbicacion] = useState({});
+    const [clima, setClima] = useState({});
 
+    const [mostrarResultados, setMostrarResultados] = useState(false);
 
     const APIkey = "ecdbf1dacca03202974b7c373eb3d17e";
-    let url = `http://api.openweathermap.org/geo/1.0/direct?q=Tucuman&limit=5&appid=${APIkey}`;
 
-    useEffect(() => {
-        consultarAPI();
-    }, []);
-
-    const consultarAPI = async () => {
+    const obtenerDatosUbicacionAPI = async () => {
+        let urlUbicacion = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&limit=1&appid=${APIkey}&lang=es`;
         try {
-            const respuesta = await fetch(url);
-            const dato = await respuesta.json();
-            console.log(dato)
-            setClimas(dato);
+            const ubicacionRespuesta = await fetch(urlUbicacion);
+            const datosUbicacion = await ubicacionRespuesta.json();
+            console.log(datosUbicacion)
+            setUbicacion(datosUbicacion[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const obtenerDatosClimaAPI = async (ubicacion) => {
+        let urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${ubicacion.lat}&lon=${ubicacion.lon}&appid=${APIkey}&units=metric&lang=es`;
+        try {
+            const climaRespuesta = await fetch(urlClima);
+            const datosClima = await climaRespuesta.json();
+            console.log(datosClima)
+            setClima(datosClima);
+            setMostrarResultados(true);
         } catch (error) {
             console.log(error);
         }
     };
 
+    useEffect(() => {
+        if (ubicacion.lat && ubicacion.lon) {
+            obtenerDatosClimaAPI(ubicacion);
+        }
+    }, [ubicacion]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        obtenerDatosUbicacionAPI(ciudad);
+        setMostrarResultados(false);
     }
     const handleChange = (e) => {
-        const ubicacion = e.target.value;
-        setUbicacion(ubicacion)
+        const ciudad = e.target.value;
+        setCiudad(ciudad);
+        setMostrarResultados(false);
     }
 
     return (
         <>
             <Container className="rounded-5 border-1 bg-success-subtle p-5 fs-2 my-5">
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="buscadorUbicacion">
-                        <Form.Label>Buscar el clima por Pais</Form.Label>
+                        <Form.Label>Buscar el clima por Ciudad</Form.Label>
                         <div className='d-flex gap-3'>
-                            <Form.Control type="text" value={ubicacion} placeholder="Ingrese la ubicación para consultar el clima..." onChange={handleChange} />
-                            <Button variant="outline-success" className="rounded-4" onSubmit={handleSubmit}>Buscar</Button>
+                            <Form.Control type="text" value={ciudad} placeholder="Ingrese la ciudad para consultar el clima..." onChange={handleChange} />
+                            <Button variant="outline-success" className="rounded-4">Buscar</Button>
                         </div>
                     </Form.Group>
                 </Form>
             </Container>
-            <DatosClima ubicacion={ubicacion}></DatosClima>
+            <DatosClima clima={clima} mostrarResultados={mostrarResultados}/>
         </>
 
     );
--- src/components/DatosClima.jsx
+++ src/components/DatosClima.jsx
@@ -1,27 +1,43 @@
 import { ListGroup, Container } from "react-bootstrap";
-import { useState } from "react";
 
-const DatosClima = ({ubicacion}) => {
-  const [mostrarResultados, setMostrarResultados] = useState(true);
+const DatosClima = ({clima, mostrarResultados}) => {
 
   const componenteRenderizado = mostrarResultados ? (
     <Container className="my-5">
-      <h2 className="display-2">Resultados del clima en {ubicacion}</h2>
+      <h2 className="display-2">Resultados del clima en {clima.name}</h2>
       <ListGroup className="fs-3 p-5">
         <ListGroup.Item className="py-3 text-bg-success">
-          <strong>Temperatura:</strong> {}
+          <strong>Temperatura:</strong> {clima.main.temp} °C
         </ListGroup.Item>
         <ListGroup.Item className="py-3 text-bg-success">
-          <strong>Presión:</strong> {}
+          <strong>Temperatura Máxima:</strong> {clima.main.temp_max} °C
         </ListGroup.Item>
         <ListGroup.Item className="py-3 text-bg-success">
-          <strong>Humedad:</strong> {}
+          <strong>Temperatura Mínima:</strong> {clima.main.temp_min} °C
+        </ListGroup.Item>
+        <ListGroup.Item className="py-3 text-bg-success">
+          <strong>Sensación Térmica:</strong> {clima.main.feels_like} °C
+        </ListGroup.Item>
+        <ListGroup.Item className="py-3 text-bg-success">
+          <strong>Presión:</strong> {clima.main.pressure} hPa
+        </ListGroup.Item>
+        <ListGroup.Item className="py-3 text-bg-success">
+          <strong>Humedad:</strong> {clima.main.humidity} %
+        </ListGroup.Item>
+        <ListGroup.Item className="py-3 text-bg-success">
+          <strong>Velocidad del Viento:</strong> {clima.wind.speed} m/s
+        </ListGroup.Item>
+        <ListGroup.Item className="py-3 text-bg-success">
+          <strong>Dirección del Viento:</strong> {clima.wind.deg} grados
+        </ListGroup.Item>
+        <ListGroup.Item className="py-3 text-bg-success">
+          <strong>Descripcion:</strong> {clima.weather[0].description}
         </ListGroup.Item>
       </ListGroup>
     </Container>
   ) : (
     <Container className="my-5">
-      <h2 className="display-2 text-bg-danger p-5 rounded-5">No se encontraron resultados...</h2>{" "}
+      <h2 className="display-2 text-bg-danger p-5 rounded-5">No se encontraron resultados...</h2>
     </Container>
   );
   return componenteRenderizado;
